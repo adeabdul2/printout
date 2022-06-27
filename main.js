d44cb87df7a7bdcf5ee20f7870091879e071a159
Abdulafeez Adesokan <adeshokan.a@gmail.com>
2022-06-27 18:16:24 +0100

    script

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function aboutMe(){
+    const names = window.prompt("What is your name: ");
+    const height = window.prompt("How tall are you: ");
+    const country = window.prompt("What country are you from: ");
+    alert("Hi! \nMy name is " + names + ". I am " + height + " tall and I am from " + country + ".");
+
+}
+
+aboutMe()
